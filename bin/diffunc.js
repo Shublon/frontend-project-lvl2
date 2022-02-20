6972938ace6f0a4892deb1ba2231ce620f0f2172
Artem <artem.rebrov.01@inbox.ru>
2022-02-21 01:15:42 +0300

    step 3 mod

---
+++ bin/diffunc.js
@@ -0,0 +1,50 @@
+import _ from "lodash";
+import * as fs from "fs";
+import * as path from "path";
+
+const diffunc = (filepath1, filepath2) => {
+    const afterPath1 = path.resolve("__fixture__", filepath1);
+    const afterPath2 = path.resolve("__fixture__", filepath2);
+
+    const fileRead1 = fs.readFileSync(afterPath1, { encoding: "utf8" });
+    const fileRead2 = fs.readFileSync(afterPath2, { encoding: "utf8" });
+
+    let parsedFile1 = [];
+    JSON.parse(fileRead1, (key, value) =>
+      parsedFile1.push(`${key}---${value}`)
+    );
+    const parseFile1 = parsedFile1.slice(0, -1);
+
+    const parsedFile2 = [];
+    JSON.parse(fileRead2, (key, value) =>
+      parsedFile2.push(`${key}---${value}`)
+    );
+    const parseFile2 = parsedFile2.slice(0, -1);
+
+    parseFile1.sort();
+    parseFile2.sort();
+
+    console.log("{");
+
+    for (let array of parseFile1) {
+      if (parseFile2.includes(array)) {
+        const arr = array.split("---");
+        console.log(`    ${arr[0]}: ${arr[1]}`);
+      } else {
+        const arr = array.split("---");
+        console.log(`  - ${arr[0]}: ${arr[1]}`);
+      }
+    }
+
+    for (let array of parseFile2) {
+      if (parseFile1.includes(array)) {
+        continue;
+      } else {
+        const arr = array.split("---");
+        console.log(`  + ${arr[0]}: ${arr[1]}`);
+      }
+    }
+    console.log("}");
+  };
+
+export default diffunc;
